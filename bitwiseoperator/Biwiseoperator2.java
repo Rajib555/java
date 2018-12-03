1ce2a6e99831f34a78f0088035338ae81db58a63
Rajib555 <rajibmandol01@gmail.com>
2018-12-03 10:46:21 -0800

    first commit

---
+++ bitwiseoperator/Biwiseoperator2.java
@@ -0,0 +1,26 @@
+
+package bitwiseoperator;
+
+
+
+public class Biwiseoperator2 {
+    
+    
+    public static void main(String[] args) {
+        
+        
+        int a=8;
+        int b=4;
+        
+        int c;
+        
+        c=a>>3;
+        System.out.println("a>>3 ="+c);
+        
+        c=a<<3;
+        System.out.println("a<<3 ="+c);
+        
+    }
+    
+    
+}
