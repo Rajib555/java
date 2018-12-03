35bc0c3f1a23f8c7d8eb421f5b4a2376df86de18
Rajib555 <rajibmandol01@gmail.com>
2018-12-03 10:24:38 -0800

    first commit

---
+++ bitwiseoperator/BiwiseOperator.java
@@ -0,0 +1,26 @@
+
+package bitwiseoperator;
+
+
+public class BiwiseOperator {
+    
+    public static void main(String[] args) {
+        
+        int a=8;
+        int b=4;
+        
+        int c;
+        
+        c=a&b;
+        System.out.println("a$b ="+c);
+        
+        c=a|b;
+        System.out.println("a|b ="+c);
+        
+        c=a^b;
+        System.out.println("a^b ="+c);
+        
+        
+    }
+    
+}
